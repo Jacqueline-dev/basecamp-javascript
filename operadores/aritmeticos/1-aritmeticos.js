e8b4cd34956d82e2133f400a636330b14332889d
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-02-22 11:18:04 -0300

    estudando operadores

---
+++ operadores/aritmeticos/1-aritmeticos.js
@@ -0,0 +1,29 @@
+// módulo (%)
+
+// operador binário. Retorna o inteiro restante da divisão dos dois operadores.
+
+12 & 5 // retorna 2.
+
+// incremento (++) e Decremento (--)
+++x 
+x++
+
+const a = ++2 //3
+const b = 2++ //2
+
+--x
+x--
+
+// negação (- e Adição(+))
+-3
++ "3" // retorna 3
++true //retorna 1
++false //retorna 0
+-true // retorna -1
+
+// operador de exponenciação (**)
+2 ** 3 //retorna 8
+10 ** -1 // retorna 0.1
+
+// operador de agrpamento
+2 (2 +3)
---
+++ operadores/atribuição/1-atribuicao.js
@@ -0,0 +1,19 @@
+// atribuição
+x = y
+
+// atribuição de adição
+x = x + y // ou
+x += y
+
+// atribuição de subtração
+x = x - y //ou
+x -= y 
+
+// atribuição de multiplicação
+
+x = x * y //ou
+x *= y
+
+// atribuição de resto
+x = x % y //ou
+x %= y
