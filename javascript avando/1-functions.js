193890aa43bee303e09cd1a1fccedb1a2ec39001
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-02-23 15:11:39 -0300

    estudando funtions modernas

---
+++ javascript avando/1-functions.js
@@ -0,0 +1,34 @@
+var obj = {
+  showContext: function showContext() {
+    this.log('teste')
+
+    setTimeout(
+      function () {
+        console.log(this)
+      }.bind(this),
+      1000
+    )
+  },
+  log: function log(value) {
+    console.log(value) 
+  }
+}
+obj.showContext().
+
+
+
+// forma moderna
+var obj = {
+  showContext: function showContext() {
+    this.log('teste')
+
+    setTimeout(() => {
+        this.log('after 100ms')
+      }, 1000)
+  },
+  log: function log(value) {
+    console.log(value) 
+  }
+}
+obj.showContext()
+
